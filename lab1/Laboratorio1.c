b43b7f4141519c8a5eaaee0a89ce884f91d6f8c3
Giorgio Rengucci <4483986@studenti.unige.it>
2022-04-12 22:55:04 +0200

    added matrix lib

--- lab1/Laboratorio1.c
+++ lab1/Laboratorio1.c
@@ -18,10 +18,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <time.h>
 //#include <pthread.h>
 #include "pthread_barrier.h"
+#include "matrix_util.h"
 
 typedef struct {
   float **matrixA;
@@ -34,34 +34,8 @@ typedef struct {
   int row_end;
 } thread_param_t;
 
+// global barrier
 pthread_barrier_t barrier;
-
-float** createMatrix(int rows, int cols){
-  //create the matrix
-  float** matrix = (float**) malloc(rows * sizeof(float*));
-  for(int i = 0;i<rows;i++) {
-    matrix[i] = (float*) malloc(cols * sizeof(float));
-  }
-
-  //fill the matrix with random values
-  for(int i = 0;i<rows;i++) {
-    for(int j = 0;j<cols;j++) {
-      matrix[i][j] = (float)rand()/(float)(RAND_MAX/10.0);
-    }
-  }
-
-  return matrix;
-}
-
-float** createMatrixOfZeros(int rows, int cols){
-  //create the matrix
-  float** matrix = (float**) malloc(rows * sizeof(float*));
-  for(int i = 0;i<rows;i++) {
-    matrix[i] = (float*) malloc(cols * sizeof(float));
-    memset(matrix[i], 0, cols*sizeof(float));
-  }
-  return matrix;
-}
 
 thread_param_t* createParameters(int* rows_for_threads, float** matrixA, int rowsA, int colsA, float** matrixB, int colsB, float** matrixAB, int threads_num ){
   thread_param_t * my_args = (thread_param_t*) malloc(threads_num * sizeof(thread_param_t));
@@ -76,18 +50,8 @@ thread_param_t* createParameters(int* rows_for_threads, float** matrixA, int row
   return my_args;
 }
 
-void printMatrix(float **matrix, int rows, int cols) {
-  for (int i = 0; i < rows; i++) {
-    printf("[ ");
-    for (int j = 0; j < cols; j++) {
-      j == cols - 1 ? printf("%f ]\n", matrix[i][j]) : printf("%f | ", matrix[i][j]);
-    }
-  }
-  printf("\n");
-}
-
-int* rowsForThread(int rows, int threads_num){
-
+int* rowsForThread(int rows, int threads_num)
+{
   int* rowsForThread = (int*) malloc(threads_num * sizeof(int));
   memset(rowsForThread, 0, threads_num*sizeof(rowsForThread[0]));
   int i = 0;
