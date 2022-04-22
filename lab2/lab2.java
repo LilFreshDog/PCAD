adfd14e55092deb3e6f6359e61e94b4e6232f6ed
Giorgio Rengucci <4483986@studenti.unige.it>
2022-04-22 09:49:55 +0200

    first commit lab2.java

---
+++ lab2/lab2.java
@@ -0,0 +1,9 @@
+/**
+ * LABORATORIO 2 - Calcolo dei numeri primi tramite metodo di crivello concorrente
+ * 
+ * 👨🏼‍💻👨🏻‍💻👨🏼‍💻
+ * @author Milo Galli (4793560@studenti.unige.it)
+ * @author Samuele Pignone (4838155@studenti.unige.it)
+ * @author Giorgio Rengucci (4483986@studenti.unige.it)
+ *
+ */
