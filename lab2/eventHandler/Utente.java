a400346d3efe2ecebcf737470dc5b7f6ba433fcb
Samuele Pignone <s4838155@studenti.unige.it>
2022-04-27 10:16:46 +0200

    Aggiunta metodi utente

--- lab2/eventHandler/Utente.java
+++ lab2/eventHandler/Utente.java
@@ -1,7 +1,21 @@
 package lab2.eventHandler;
 
 public class Utente extends Thread {
-  public void run() {
-    //simulare il comportamento di un utente
+  private Evento evento;
+  private Integer postiDaPrenotare;
+
+  public Utente(Evento evento, Integer postiDaPrenotare){
+    this.evento = evento;
+    this.postiDaPrenotare = postiDaPrenotare;
   }
+
+  public void run() {
+    prenotaPosti(evento, postiPrenotati)
+  }
+
+  // method for the user to reserve a seat by calling the reserve method of the event
+  public Boolean prenotaPosti() {
+    return evento.prenotaPosti(postiDaPrenotare);
+  }
+
 }
