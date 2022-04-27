b222ff26d9040bb60308fa6cfb8221d7efb2d7cf
Samuele Pignone <s4838155@studenti.unige.it>
2022-04-27 10:50:29 +0200

    Update Test.java

--- lab2/eventHandler/Test.java
+++ lab2/eventHandler/Test.java
@@ -3,9 +3,13 @@ import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Test {
-    Integer numeroUtenti = 10;
-    listaEventi = new ArrayList<String>();
-    postiEventi = new ArrayList<Integer>();
+    Integer numeroUtenti = Runtime.getRuntime().availableProcessors();
+    System.out.println("Numero utenti: " + numeroUtenti);
+    ArrayList<Utente> utenti = new ArrayList<Utente>();
+
+
+    ArrayList<String> listaEventi = new ArrayList<String>();
+    ArrayList<Integer> postiEventi = new ArrayList<Integer>();
     listaEventi.add("Coachella");
     postiEventi.add(300000);
     listaEventi.add("Tomorrowland");
@@ -17,7 +21,6 @@ public class Test {
     listaEventi.add("Lezione di PCAD");
     postiEventi.add(100);
 
-
     Eventi eventi = new Eventi();
     // Aggiungo gli aventi alla classe eventi
     for (int i = 0; i < listaEventi.size(); i++){
@@ -26,7 +29,17 @@ public class Test {
 
     for (int i = 0; i < numeroUtenti; i++){
         Utente utente = new Utente(eventi.Eventi.get( ThreadLocalRandom.current().nextInt(0, listaEventi.size()) ), ThreadLocalRandom.current().nextInt(1, 20));
+        utenti.add(utente);
         utente.start();
     })
-    
+
+    // join threads
+    for (int i = 0; i < numeroUtenti; i++){
+        try {
+            utenti.get(i).join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
