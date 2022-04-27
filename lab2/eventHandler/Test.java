ae6e4fef7a3f1189452fd25e2f5e77123464160e
Samuele Pignone <s4838155@studenti.unige.it>
2022-04-27 11:01:28 +0200

    Update Test.java

--- lab2/eventHandler/Test.java
+++ lab2/eventHandler/Test.java
@@ -3,42 +3,52 @@ import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Test {
-    Integer numeroUtenti = Runtime.getRuntime().availableProcessors();
-    System.out.println("Numero utenti: " + numeroUtenti);
     ArrayList<Utente> utenti = new ArrayList<Utente>();
-
-
-    ArrayList<String> listaEventi = new ArrayList<String>();
+    ArrayList<String> nomiEventi = new ArrayList<String>();
     ArrayList<Integer> postiEventi = new ArrayList<Integer>();
-    listaEventi.add("Coachella");
-    postiEventi.add(300000);
-    listaEventi.add("Tomorrowland");
-    postiEventi.add(500000);
-    listaEventi.add("Unigeparty");
-    postiEventi.add(200);
-    listaEventi.add("Conferenza sui thread di java");
-    postiEventi.add(50);
-    listaEventi.add("Lezione di PCAD");
-    postiEventi.add(100);
-
     Eventi eventi = new Eventi();
-    // Aggiungo gli aventi alla classe eventi
-    for (int i = 0; i < listaEventi.size(); i++){
-        eventi.Crea(listaEventi.get(i), postiEventi.get(i));
+
+    public Test(){
+        this.utenti = new ArrayList<Utente>();
+        this.nomiEventi = new ArrayList<String>();
+        this.postiEventi = new ArrayList<Integer>();
     }
 
-    for (int i = 0; i < numeroUtenti; i++){
-        Utente utente = new Utente(eventi.Eventi.get( ThreadLocalRandom.current().nextInt(0, listaEventi.size()) ), ThreadLocalRandom.current().nextInt(1, 20));
-        utenti.add(utente);
-        utente.start();
-    })
+    private initializeList(){
+        Integer numeroUtenti = Runtime.getRuntime().availableProcessors();
+        System.out.println("Numero utenti: " + numeroUtenti);
 
-    // join threads
-    for (int i = 0; i < numeroUtenti; i++){
-        try {
-            utenti.get(i).join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        nomiEventi.add("Coachella");
+        postiEventi.add(300000);
+        nomiEventi.add("Tomorrowland");
+        postiEventi.add(500000);
+        nomiEventi.add("Unigeparty");
+        postiEventi.add(200);
+        nomiEventi.add("Conferenza sui thread di java");
+        postiEventi.add(50);
+        nomiEventi.add("Lezione di PCAD");
+        postiEventi.add(100);
+    }
+
+    public static void startTesting(String[] args) {
+        // Aggiungo gli aventi alla classe eventi
+        for (int i = 0; i < nomiEventi.size(); i++){
+            eventi.Crea(nomiEventi.get(i), postiEventi.get(i));
+        }
+
+        for (int i = 0; i < numeroUtenti; i++){
+            Utente utente = new Utente(eventi.Eventi.get( ThreadLocalRandom.current().nextInt(0, nomiEventi.size()) ), ThreadLocalRandom.current().nextInt(1, 20));
+            utenti.add(utente);
+            utente.start();
+        })
+
+        // join threads
+        for (int i = 0; i < numeroUtenti; i++){
+            try {
+                utenti.get(i).join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
