d16d86be2bdcfbd933363b899771c457061fab7c
Giorgio Rengucci <4483986@studenti.unige.it>
2022-04-27 12:19:27 +0200

    modificata classe utenti

--- lab2/eventHandler/Utente.java
+++ lab2/eventHandler/Utente.java
@@ -1,22 +1,28 @@
 package lab2.eventHandler;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Utente extends Thread {
-  private Evento evento;
-  private Integer postiDaPrenotare;
 
-  public Utente(Evento evento, Integer postiDaPrenotare){
-    this.evento = evento;
-    this.postiDaPrenotare = postiDaPrenotare;
+  private BlockingQueue<Evento> eventi;
+
+  public Utente(BlockingQueue<Evento> eventi){
+    this.eventi = eventi;
   }
 
   public void run() {
-    prenotaPosti(evento, postiPrenotati)
+    try {
+      eventi.Prenota(eventi.Eventi.get(ThreadLocalRandom.current().nextInt(0, eventi.getNumEventi())), ThreadLocalRandom.current().nextInt(1, 20));
+      TimeUnit.SECONDS.sleep(500);
+      eventi.Prenota(eventi.Eventi.get(ThreadLocalRandom.current().nextInt(0, eventi.getNumEventi())), ThreadLocalRandom.current().nextInt(1, 20));
+      TimeUnit.SECONDS.sleep(500);
+      eventi.Prenota(eventi.Eventi.get(ThreadLocalRandom.current().nextInt(0, eventi.getNumEventi())), ThreadLocalRandom.current().nextInt(1, 20));
+      TimeUnit.SECONDS.sleep(500);
+      eventi.Prenota(eventi.Eventi.get(ThreadLocalRandom.current().nextInt(0, eventi.getNumEventi())), ThreadLocalRandom.current().nextInt(1, 20));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
-
-  // method for the user to reserve a seat by calling the reserve method of the event
-  public Boolean prenotaPosti() {
-    System.out.println("Provo a prenotare " + postiDaPrenotare + " posti per l'evento " + evento.getNome());
-    return evento.prenotaPosti(postiDaPrenotare);
-  }
-
 }
