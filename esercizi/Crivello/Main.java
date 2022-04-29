77e99ec7cf9e6257b919d1e0c150699427096614
Giorgio Rengucci <4483986@studenti.unige.it>
2022-04-29 09:49:09 +0200

    aggiunto esercizio crivello

---
+++ esercizi/Crivello/Main.java
@@ -0,0 +1,20 @@
+package Crivello;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    System.out.print("\nCalcola i numeri primi fino a : ");
+    int N = input.nextInt();
+    input.close();
+    
+    Crivello crivello = new Crivello(3);
+    crivello.start();
+
+    System.out.println("\n");
+
+    for(int i = 3; i <= N; i++)if( i%2 != 0)crivello.push(i);
+    crivello.push(-1);
+    crivello.interrupt();
+  }  
+}
