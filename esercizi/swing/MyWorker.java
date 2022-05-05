88a1b2ebb5f70822bdf664ac0ea7a118c723227a
Milo Galli <42500999+thaMilo@users.noreply.github.com>
2022-05-05 11:40:12 +0200

    Add files via upload

---
+++ esercizi/swing/MyWorker.java
@@ -0,0 +1,34 @@
+package swing;
+import javax.swing.*;
+import java.util.List;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ExecutionException;
+
+public class MyWorker extends SwingWorker<String, Integer> {
+
+    GUI mygui;
+
+    public MyWorker(GUI mygui) {
+        this.mygui = mygui;
+    }
+
+
+    @Override
+    protected String doInBackground() throws Exception {
+        Thread.sleep(1000); 
+        return "Done!";
+    }
+    
+    @Override
+    protected void process(List<Integer> chunks) {
+        
+    }
+
+    @Override
+    protected void done() {
+    	
+    }
+
+}
