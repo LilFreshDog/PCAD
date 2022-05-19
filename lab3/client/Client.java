a0a738ae4f988e9b696d44561208530f63a00a8d
Giorgio Rengucci <4483986@studenti.unige.it>
2022-05-19 14:55:27 +0200

    added client class, use it to extend admin & user

---
+++ lab3/client/Client.java
@@ -0,0 +1,54 @@
+package lab3.client;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+	protected Socket socket;
+	protected String hostname = "localhost";
+	protected int port = 8123;
+	protected PrintWriter out;
+	protected BufferedReader in;
+
+	public Client(String hostname, int port){
+		try {
+			socket = new Socket(hostname, port);
+		} catch (Exception e) {
+			System.out.println("Error connecting to "+hostname);
+			System.exit(1);
+		}
+
+		try {
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		} catch (Exception e) {
+			System.out.println("Error creating streams");
+			System.exit(1);
+		}
+	}
+
+	protected String sendMessage(String msg) {
+	out.println(msg);
+	try {
+		return in.readLine();
+	} catch (Exception e) {
+		System.out.println("Error reading message");
+		System.exit(1);
+	}
+	return null;
+	}
+
+	public void stopConnection(){
+		try {
+			in.close();
+			out.close();
+			socket.close();
+		} catch (IOException e) {
+			System.out.println("Error closing connection");
+			System.exit(1);
+		}
+	}
+}
