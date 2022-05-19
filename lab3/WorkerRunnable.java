cf1a9e57096200041eddc62afd02739fb02caab0
thaMilo <milougo.galli@gmail.com>
2022-05-19 17:00:15 +0200

    lista eventi fatta

--- lab3/WorkerRunnable.java
+++ lab3/WorkerRunnable.java
@@ -20,8 +20,7 @@ public class WorkerRunnable implements Runnable {
 			BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
 			//implemnto le operazioni del worker
-			parseCommand(serverMessage);
-			output.println("200 ok SOOS");
+			parseCommand(serverMessage, output);
 			output.close();
 			input.close();
 		} catch (IOException e) {
@@ -29,37 +28,48 @@ public class WorkerRunnable implements Runnable {
 		}
  	}
 
-	private void parseCommand(String command){
+	private void parseCommand(String command, PrintWriter output){
 		String[] mycommand = command.split("|");
-		if(mycommand[0].equals("crea"))creaEvento(mycommand[1], Integer.parseInt(mycommand[2]));
-		if(mycommand[0].equals("prenota"))prenotaEvento(mycommand[1], Integer.parseInt(mycommand[2]));
-		if(mycommand[0].equals("cancella"))cancellaEvento(mycommand[1]);
-		if(mycommand[0].equals("chiudi"))chiudiPrenotazioni(mycommand[1]);
-		if(mycommand[0].equals("aggiungi"))aggiungiPosti(mycommand[1], Integer.parseInt(mycommand[2]));;
-		if(mycommand[0].equals("lista"))listaEventi();	
+		if(mycommand[0].equals("crea"))creaEvento(mycommand[1], Integer.parseInt(mycommand[2]),output );
+		if(mycommand[0].equals("prenota"))prenotaEvento(mycommand[1], Integer.parseInt(mycommand[2]),output);
+		if(mycommand[0].equals("cancella"))cancellaEvento(mycommand[1], output);
+		if(mycommand[0].equals("chiudi"))chiudiPrenotazioni(mycommand[1], output);
+		if(mycommand[0].equals("aggiungi"))aggiungiPosti(mycommand[1], Integer.parseInt(mycommand[2]), output);;
+		if(mycommand[0].equals("lista"))listaEventi(output);	
 	}
 
-	private void creaEvento(String nome, Integer posti){
+	private void creaEvento(String nome, Integer posti, PrintWriter output){
     eventi.nuovoEvento(nome, posti);
+		output.println("200 ok SOOS");
   }
 	
-	private void prenotaEvento(String nome, Integer posti){
+	private void prenotaEvento(String nome, Integer posti, PrintWriter output){
     eventi.Prenota("PIZZA",nome, posti);
+		output.println("200 ok SOOS");
   }
 
-	private void aggiungiPosti(String nome, Integer posti){
+	private void aggiungiPosti(String nome, Integer posti, PrintWriter output){
     eventi.aggiungiPosti(nome, posti);
+		output.println("200 ok SOOS");
   }
 
-  private void cancellaEvento(String nome){
+  private void cancellaEvento(String nome, PrintWriter output){
     eventi.Cancella(nome);
+		output.println("200 ok SOOS");
   }
 
-  private void chiudiPrenotazioni(String nome){
+  private void chiudiPrenotazioni(String nome, PrintWriter output){
     eventi.Chiudi(nome);
+		output.println("200 ok SOOS");
   }
 
-	private void listaEventi(){
+	private void listaEventi(PrintWriter output){
+		String toReturn = "";
+		for (String key : eventi.eventi.keySet()) {
+     	toReturn = toReturn + key + "&" + eventi.eventi.get(key).getPosti() + "|";
+    }
 
+		toReturn = toReturn.substring(0, toReturn.length()-1);
+		output.println(toReturn);
   }
 }
