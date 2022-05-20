d92bc684afc77fc58e41d1972a82b77c4557ac1e
Samuele Pignone <s4838155@studenti.unige.it>
2022-05-20 09:27:21 +0200

    Ripartiamo con la GUI

--- lab3/client/user/EventoTableModel.java
+++
@@ -1,4 +0,0 @@
-package lab3.client.user;
-public class EventoTableModel {
-    
-}
--- lab3/client/user/Main.java
+++
@@ -1,7 +0,0 @@
-package lab3.client.user;
-
-public class Main {
-    public static void main(String[] args){
-		new userGUI();
-	}
-}
