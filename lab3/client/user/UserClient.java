1ea0382ad248fd313a26244f4fff9e001cb1d6a8
thaMilo <milougo.galli@gmail.com>
2022-05-25 17:33:25 +0200

    User constructor now connects to the GUI

--- lab3/client/user/UserClient.java
+++ lab3/client/user/UserClient.java
@@ -3,9 +3,13 @@ package lab3.client.user;
 import lab3.client.Client;
 
 public class UserClient extends Client {
+
 	public UserClient(String hostname, int port) {
 		super(hostname, port);
+		UserGUI gui = new UserGUI(this);
+		gui.display();
 	}
+	
 	public String prenota(String evento, int posti) {
 		return sendMessage("prenota|" + evento + "|" + posti);
 	}
